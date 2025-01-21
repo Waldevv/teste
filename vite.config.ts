d5f36b0d5f2151fc6267cacc7bfc684e5148cfb3
Waldevv <133806522+Waldevv@users.noreply.github.com>
2025-01-21 16:29:35 -0400

    Deploy do build na branch main

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/thinklib-documentation/', // Use o nome do seu repositório no GitHub
+  base: '/thinklib-documentation/', // Substitua pelo nome exato do repositório no GitHub
   plugins: [react()],
-})
+});
